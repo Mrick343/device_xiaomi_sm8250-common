b24f5378591f2a70f3d5fc71b5802594aca675b9
orges <me@orgesified.ml>
2023-01-11 14:57:41 +0000

    sm8250-common: Set BOARD_USES_ADRENO to true  * libc2dconvert wont be included unless this is set
    Change-Id: I55be5eefb145270d5d79036ea5eebeedad65770e
    Signed-off-by: SenseiiX <senseixdev@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -76,6 +76,7 @@ TARGET_CAMERA_SERVICE_EXT_LIB := //$(COMMON_PATH):libcameraservice_extension.xia
 $(call soong_config_set,camera,override_format_from_reserved,true)
 
 # Display
+BOARD_USES_ADRENO := true
 TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS := 0x546C00000000
 TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE := true
 TARGET_NO_RAW10_CUSTOM_FORMAT := true
